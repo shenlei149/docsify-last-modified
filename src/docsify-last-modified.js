1446828bd37eed6e3e57643bc28097ab032dc5fb
Leo SHEN <shenlei149@outlook.com>
2022-10-10 14:16:33 +0800

    Update docsify-last-modified.js

--- src/docsify-last-modified.js
+++ src/docsify-last-modified.js
@@ -14,26 +14,33 @@ var install = function (hook, vm) {
     CONFIG.dateFormat = opts.dateFormat && typeof opts.dateFormat === 'string' ? opts.dateFormat : CONFIG.dateFormat;
     CONFIG.whereToPlace = opts.whereToPlace && typeof opts.whereToPlace === 'string' ? opts.whereToPlace : CONFIG.whereToPlace;
 
-    hook.beforeEach(function(html) {
+    hook.beforeEach(async function (markdown, next) {
         var time = '{docsify-updated}';
+        var text = '';
         if (CONFIG.repo !== '') {
-            var date_url = 'https://api.github.com/repos/' + CONFIG.repo + '/commits?per_page=1';
-            
-            if (CONFIG.basePath !== '') {
-                date_url = date_url + '&path=' + CONFIG.basePath + vm.route.file;
-            }
-            fetch(date_url).then((response) => {
-              return response.json();
-            }).then((commits) => {
-                if (commits.length > 0) {
-                    time = tinydate(CONFIG.dateFormat)(new Date(commits[0]['commit']['committer']['date']));
-                }
-            });
-        }
-        
-        var text = CONFIG.preString + time;
+            try {
+                var date_url = 'https://api.github.com/repos/' + CONFIG.repo + '/commits?per_page=1';
 
-        return 'top' !== CONFIG.whereToPlace ? html + '\n\n' + text : text + '\n\n' + html;
+                if (CONFIG.basePath !== '') {
+                  date_url = date_url + '&path=' + CONFIG.basePath + vm.route.file;
+                }
+
+                await fetch(date_url).then((response) => {
+                    return response.json();
+                }).then((commits) => {
+                    console.error(commits);
+                    if (commits.length > 0) {
+                        time = tinydate(CONFIG.dateFormat)(new Date(commits[0]['commit']['committer']['date']));
+                    }
+                });
+            } catch (err) {
+                text = err;
+            }
+        }
+
+        text = CONFIG.preString + time;
+        markdown = 'top' !== CONFIG.whereToPlace ? markdown + '\n\n' + text : text + '\n\n' + markdown;
+        next(markdown);
     });
 };
 
